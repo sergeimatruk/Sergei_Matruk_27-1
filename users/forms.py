0b1289449e93451c4aa82353f74a958014345900
sergejmatruk <sergey.matruk1@gmail.com>
2023-05-03 11:16:58 +0600

    HW 6

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=32)
+    password1 = forms.CharField(widget=forms.PasswordInput(), min_length=3)
+    password1 = forms.CharField(widget=forms.PasswordInput(), min_length=3)
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=32)
+    password1 = forms.CharField(widget=forms.PasswordInput(), min_length=3)
