d5f02cdd335722cf4cb9da88fb738b86644a0462
sergejmatruk <sergey.matruk1@gmail.com>
2023-05-03 11:16:43 +0600

    HW 6

--- Sergei_Matruk_27_1/urls.py
+++ Sergei_Matruk_27_1/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 from Sergei_Matruk_27_1 import settings
 from posts.views import main_view, products_view, product_detail_view
-from django.conf.urls.static import static
+from users.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_view),
     path('products/', products_view),
-    path('products/<int:id>', product_detail_view)
+    path('products/<int:id>', product_detail_view),
+    path('users/register/', register_view),
+    path('users/login/', login_view),
+    path('users/logout/', logout_view)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -10,7 +10,8 @@ def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
         context = {
-            'products' : products
+            'products': products,
+            'user': request.user
         }
         return render(request, 'products/products.html', context=context)
 
